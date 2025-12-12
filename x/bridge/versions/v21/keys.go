b40713059b75502be2145c3efb59361d9d3dda77
Eduard Mikhrin <48176280+EduardMikhrin@users.noreply.github.com>
2025-12-12 13:10:46 +0200

    Removed old types versions (#126)

--- x/bridge/versions/v21/keys.go
+++
@@ -1,9 +0,0 @@
-package v21
-
-const (
-	// ModuleName defines the module name
-	ModuleName = "bridge"
-
-	// RouterKey defines the module's message routing key
-	RouterKey = ModuleName
-)
--- x/bridge/versions/v24/keys.go
+++
@@ -1,9 +0,0 @@
-package v24
-
-const (
-	// ModuleName defines the module name
-	ModuleName = "bridge"
-
-	// RouterKey defines the module's message routing key
-	RouterKey = ModuleName
-)
