392fb4b3c705e094ae8aa61647f8fd014d6e9c92
MarkCherepovskyi <mark.cherepovskyi@gmail.com>
2026-02-19 11:22:55 +0200

    update handler v12.1.30-rc2

--- app/app.go
+++ app/app.go
@@ -1046,6 +1046,13 @@ func NewBridge(
 		},
 	)
 
+	app.UpgradeKeeper.SetUpgradeHandler(
+		"v12.1.30-rc2",
+		func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+			return app.mm.RunMigrations(ctx, app.configurator, fromVM)
+		},
+	)
+
 	if loadLatest {
 		if err := app.LoadLatestVersion(); err != nil {
 			tmos.Exit(err.Error())
